7992f2bc4c71aab5a87851728c0c8a1ca098f921
johnathan melo neto <jmmn.mg@gmail.com>
2021-03-27 18:43:16 -0300

    feat: aula 6

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,7 +1,7 @@
 import { AccountModel } from '../models/account-model'
 
-type AuthenticationParams = {
-    name: string
+export type AuthenticationParams = {
+    email: string
     password: string
 }
 
