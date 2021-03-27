96ea7de74a452ec361a522a53072c423d29efa7d
johnathan melo neto <jmmn.mg@gmail.com>
2021-03-27 18:01:00 -0300

    feat: aula 5

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,25 @@
+import { HttpPostClientSpy } from '../../test/mock-http-client'
+import { RemoveAuthentication } from './remove-authentication'
+
+type SutTypes = {
+    sut: RemoveAuthentication
+    httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoveAuthentication(url, httpPostClientSpy)
+    return {
+        sut,
+        httpPostClientSpy
+    }
+}
+
+describe('RemoteAuthentication', () => {
+    test('Should call HttpPostClient with corret URL', async () => {
+        const url = 'other_url'
+        const { sut, httpPostClientSpy } = makeSut(url)
+        await sut.auth()
+        expect(httpPostClientSpy.url).toBe(url)
+    })
+})
